6dba8c2db63e4265a68b56017342f22c0037fc0c
leesdeok98 <tjdejr147@naver.com>
2025-05-22 02:18:00 +0900

    페이드인아웃 수정

--- Assets/Script/ImageBoard/storyManager.cs
+++ Assets/Script/ImageBoard/storyManager.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class StoryManager : MonoBehaviour
 {
     [Header("이미지")]
     [SerializeField] private Image image; //SerializeField : private를 public으로 만들어주는 거
-    [SerializeField] private Image textimage;
     [SerializeField] private TMP_Text text;
 
     [Header("스크립터블")]
@@ -38,6 +38,7 @@ public class StoryManager : MonoBehaviour
             // Fade In 효과로 시작
             StartCoroutine(FadeEffect.Instance.Fade(1, 0));
         }
+        
 
     }
     void Update()
@@ -48,31 +49,40 @@ public class StoryManager : MonoBehaviour
             {
                 //Space 첫 호출 시, 이미지 및 텍스트 설정 후 타이핑 시작
                 ShowNextImage();
+                
             }
            else if (isTyping)
             {
+
                 // Space 두번째 호출 시, 이미 타이핑 중이면 타이핑 즉시 완료
                 CompIeteTyping();
             }
            else
             {
                 // 타이핑 완료 후 Space 호출 시, 다음 이미지로 전환 준비
-                isFading = true;
+                ShowNextImage();
             }
         }
     }  
     private void ShowNextImage()
     {
+        //이미지 배열 범위를 벗어나지 않도록
         if(currentImage >= images.Length)
         {
-            return;
+            Debug.Log("모든 이미지가 표시되었습니다, 다음 씬으로 전환합니다");
+            if (FadeEffect.Instance != null)
+            { 
+                StartCoroutine(FadeOutAndLoadScene("GameView"));
+            }
+
+                return;
         }
         if (FadeEffect.Instance != null)
         {
             StartCoroutine(FadeEffect.Instance.Fade(1, 0));
         }
         else
-        { 
+        {
             
         }
 
@@ -105,6 +115,26 @@ public class StoryManager : MonoBehaviour
     }
     private void CompIeteTyping()
     {
-
+        if (typingCorutine != null)
+        {
+            StopCoroutine(typingCorutine); //타이핑 코루틴 중지
+            
+        }
+        // 전체 대사 즉시 표시
+        if(currentImage > 0 && currentImage <= images.Length)
+        {
+            text.text = images[currentImage - 1].storyText;
+        }
+        //타이핑 상태 변경
+        isTyping = false;
     }
+
+    private IEnumerator FadeOutAndLoadScene(string sceneName)
+    {
+        // FadeEffect에서 Fade Out 효과 불러오기
+        yield return StartCoroutine(FadeEffect.Instance.Fade(0, 1));
+        // Pade Out 완료 후 씬 전환
+        SceneManager.LoadScene("GameView");
+    }
+   
 }
