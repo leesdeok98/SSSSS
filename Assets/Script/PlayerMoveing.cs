e6e2222c2b7b4301edbc836bb22c8e3b84087f72
leesdeok98 <tjdejr147@naver.com>
2025-05-21 21:04:47 +0900

    이게진짜야

--- Assets/Script/PlayerMoveing.cs
+++ Assets/Script/PlayerMoveing.cs
@@ -17,27 +17,22 @@ public class PlayerMoveing : MonoBehaviour
     public int jumpLevel = 2;
     private bool isHit = false;
 
-    // 상태 상수 정의
-    const int STATE_IDLE = 0;
-    const int STATE_JUMP = 1;
-    const int STATE_SLIDE = 2;
-    const int STATE_HIT = 3;
-
+    // Start is called before the first frame update
     void Start()
     {
         SetGroundTrue();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (!isHit) // 피격 중이 아닐 때만 조작 가능
-        {
-            Slide();
+        if (isHit) return; // 피격 중엔 조작 금지
 
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Invoke("TryJump", 0.1f); // 0.1초 딜레이 후 점프
-            }
+        Slide();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Invoke("TryJump", 0.1f); // 0.1초 딜레이 후 점프 시도
         }
     }
 
@@ -46,7 +41,7 @@ public class PlayerMoveing : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             SetGroundTrue();
-            PlayerAnimator.SetInteger("State", STATE_IDLE);
+            PlayerAnimator.SetInteger("State", 0);
             isGround = true;
             isJumping = false;
             jumpCount = 0;
@@ -55,10 +50,27 @@ public class PlayerMoveing : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.CompareTag("Enemy") && !isHit)
+        if (collider.gameObject.CompareTag("Enemy") && !isHit)
         {
             Hit();
         }
+    }
+
+    void Hit()
+    {
+        isHit = true;
+        PlayerAnimator.SetInteger("State", 3); // Hit 애니메이션 재생
+        rb.velocity = Vector2.zero; // 피격 시 멈추기
+
+        Debug.Log("Crush");
+
+        Invoke("RecoverFromHit", 0.7f); // 0.7초 후 회복
+    }
+
+    void RecoverFromHit()
+    {
+        isHit = false;
+        PlayerAnimator.SetInteger("State", 0); // 다시 Idle 상태로
     }
 
     void SetGroundTrue()
@@ -68,10 +80,12 @@ public class PlayerMoveing : MonoBehaviour
 
     void TryJump()
     {
+        if (isHit) return; // 피격 중 점프 금지
+
         if (isGround)
         {
             rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
-            PlayerAnimator.SetInteger("State", STATE_JUMP);
+            PlayerAnimator.SetInteger("State", 1);
             isGround = false;
             isJumping = true;
             jumpCount++;
@@ -81,36 +95,24 @@ public class PlayerMoveing : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, 0f);
             rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
             isJumping = false;
-            PlayerAnimator.Play("Jump", 0, 0f); // 점프 애니메이션 재시작
+            Animator animator = GetComponent<Animator>();
+            animator.Play("Jump", 0, 0f);
         }
+
         Debug.Log("Jump (delayed)");
     }
 
     void Slide()
     {
+        if (isHit) return; // 피격 중 슬라이드 금지
+
         if (Input.GetKey(KeyCode.C) && isGround)
         {
-            PlayerAnimator.SetInteger("State", STATE_SLIDE);
+            PlayerAnimator.SetInteger("State", 2);
         }
         else if (isGround)
         {
-            PlayerAnimator.SetInteger("State", STATE_IDLE);
+            PlayerAnimator.SetInteger("State", 0);
         }
-    }
-
-    void Hit()
-    {
-        isHit = true;
-        PlayerAnimator.SetInteger("State", STATE_HIT);
-        Debug.Log("Hit!");
-
-        // 일정 시간 후 피격 상태 해제 및 Idle 복귀
-        Invoke("RecoverFromHit", 2.0f); // Hit 애니메이션 길이에 맞게 조절
-    }
-
-    void RecoverFromHit()
-    {
-        isHit = false;
-        PlayerAnimator.SetInteger("State", STATE_IDLE);
     }
 }
