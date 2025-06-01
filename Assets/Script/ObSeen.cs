a8b52ad9ae1a2506d442d0b9111728c60776cf1c
leesdeok98 <tjdejr147@naver.com>
2025-06-01 21:45:16 +0900

    장애물 배치
    1챕터 완료 2챕터 배치 중

--- Assets/Script/ObSeen.cs
+++ Assets/Script/ObSeen.cs
@@ -1,34 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.SceneManagement;
 
 public class ObSeen : MonoBehaviour
 {
     public float speed = 5f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector2 moveDirection = Vector2.left;
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        transform.Translate(moveDirection * speed * Time.deltaTime);
     }
 
-    //private void OnTriggerEnter2D(Collider2D other)
-    //{
-    //    if (other.CompareTag("Player"))
-    //    {
-    //        Invoke("NextScene", 3f);
-    //    }
-    //}
-
-    //public void NextScene()
-    //{
-        
-    //    SceneManager.LoadScene("GameView2");
-    //}
+    public void SetDirection(Vector2 direction)
+    {
+        moveDirection = direction.normalized;
+    }
 }
---
+++ Assets/Script/StopManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class StopManager : MonoBehaviour
+{
+    public float speed = 5f;
+    private Vector2 moveDirection = Vector2.left;
+
+    void Update()
+    {
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            moveDirection = -moveDirection;
+            Debug.Log("플레이어와 충돌! 이동 방향 반전");
+        }
+    }
+
+}
