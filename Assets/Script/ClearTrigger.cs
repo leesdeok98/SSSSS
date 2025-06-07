9e9de48302f305e35518f2f954b7c30ee5ffa77c
wayven1234 <hanjun9035@naver.com>
2025-06-08 07:49:51 +0900

    ㅎㅇㅎㅇ 또 뵙겠습니다 ㅋㅋ
    clear 도와줬어요..

--- Assets/Script/ClearTrigger.cs
+++ Assets/Script/ClearTrigger.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ClearTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string nextSceneName = "Clear";
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
     }
 }
